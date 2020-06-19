34c0680b4f7aecb990697a87b8f1757c964d9600
GrassoMichele <michelegrasso1@live.it>
2020-06-19 11:24:01 +0200

    Reorganization of project structure, JSON exceptions handled

---
+++ opcua-client-kafka-gateway/app.py
@@ -0,0 +1,76 @@
+import os
+import signal
+import json
+import time
+from utility.opcua_custom_lib import servers_connection, read_nodes_from_json, removing_invalid_nodes, sub_and_monitored_items_creation, check_servers_status, polling_and_monitoring_service 
+
+def main():
+	with open("config.json") as f:
+		try:
+			data = json.load(f)
+		except Exception as ex:
+			print(f"ERROR, not a valid JSON!\n{ex.__class__, ex.args}\nExit...")
+			os._exit(0)			
+
+	try:
+		servers = data["Servers"]
+		kafka_addr = data["KafkaServer"]
+		pollingRate = data["PollingRate"]
+		publishingInterval = data["PublishingInterval"]
+	except KeyError:
+		print("ERROR in JSON configuration file! Exit...")
+		os._exit(0)
+
+	security_policies_uri=['http://opcfoundation.org/UA/SecurityPolicy#None']
+	#security_policies_uri=['http://opcfoundation.org/UA/SecurityPolicy#None', 'http://opcfoundation.org/UA/SecurityPolicy#Basic128Rsa15', 'http://opcfoundation.org/UA/SecurityPolicy#Basic256', 'http://opcfoundation.org/UA/SecurityPolicy#Basic256Sha256']
+	
+	# verify json validity
+	nodes_to_handle = read_nodes_from_json(servers)
+	
+	#Servers connection
+	clients_list, working_servers = servers_connection(servers, security_policies_uri)
+	
+	print("\n"*3 , f"{'-'*60}")
+	
+	#Variables Management
+	nodes_to_handle = removing_invalid_nodes(clients_list, working_servers, nodes_to_handle)
+	
+	subs, queues = [None for i in clients_list], [None for i in clients_list]
+	
+	def signal_handler(sig, frame):
+		print("\nCLIENT STOPPED! (You pressed CTRL+C)")
+		for i, c in enumerate(clients_list):	
+			if c != None:
+				try:
+					if subs[i] != None: 
+						subs[i][0].delete()
+						print(f"\nSubscription {subs[i][0].subscription_id} deleted!")
+				except:
+					print(f"\nUnable to delete subscription {subs[i][0].subscription_id}!")		
+				try:
+					# "ns=0;i=2259" is ServerStatus node
+					c.get_node("ns=0;i=2259").get_data_value()	
+					c.disconnect()					
+					print(f"\nClient correctly disconnected from {servers[i]['address']}")
+				except:
+					print(f"\nServer {c.server_url.netloc} is probably down!")
+		os._exit(0) 
+	
+	signal.signal(signal.SIGINT, signal_handler)
+	
+	subs, queues = sub_and_monitored_items_creation(clients_list, working_servers, nodes_to_handle, subs, queues, publishingInterval)
+			
+	time.sleep(0.1)
+
+	counter = 1 
+	while(True):
+		#Check on new working servers and servers that are working no more.
+		clients_list, working_servers, nodes_to_handle, subs, queues = check_servers_status(servers, clients_list, working_servers, nodes_to_handle, subs, queues, security_policies_uri, publishingInterval)
+		print(f"\n{'*'*15} ITERATION n. {counter} {'*'*15}")
+		queues = polling_and_monitoring_service(servers, clients_list, working_servers, nodes_to_handle, queues)
+		
+		counter += 1
+		time.sleep(pollingRate)	
+			
+if __name__ == "__main__":			
+	main()
--- opcua-client-kafka-gateway/utility/opcua_custom_lib.py
+++ opcua-client-kafka-gateway/utility/opcua_custom_lib.py
@@ -1,8 +1,5 @@
-import sys, os
-import signal
-import json
+import sys
 from textwrap import dedent
-import time
 import logging
 import queue
 from opcua import Client, ua
@@ -14,10 +11,10 @@ from opcua.client import ua_client
 from opcua.ua.ua_binary import struct_from_binary
 from opcua.ua.uaerrors import BadTimeout, BadNoSubscription, BadSessionClosed
 
-def best_endpoint_selection(client, index, printable=False):
+def best_endpoint_selection(client, server, security_policies_uri, printable=False):
 	endpoints = client.connect_and_get_server_endpoints()
 
-	if printable: print(f"\n\n{'-'*10} SERVER: {servers[index]['address']} {'-'*10}")
+	if printable: print(f"\n\n{'-'*10} SERVER: {server['address']} {'-'*10}")
 	
 	best_sec_lvl = -1
 	for i, endpoint in enumerate(endpoints):
@@ -42,7 +39,8 @@ def best_endpoint_selection(client, index, printable=False):
 		TransportProfileUri : {best_endpoint.TransportProfileUri}
 		"""))
 	return best_endpoint
-	
+
+# Andrebbe tolta in favore del json	
 def user_authentication(client):			
 	while(True):
 		auth_selection = input(dedent(
@@ -50,7 +48,7 @@ def user_authentication(client):
 		Authentication Mode:
 		1. Anonymous
 		2. Username
-		3. Certificate (not working)
+		3. Certificate
 		: """)) 
 		
 		if auth_selection == str(1):
@@ -69,42 +67,45 @@ def user_authentication(client):
 			print("\nSelection not allowed!")
 	return client
 	
-def client_connection(index, auth=False, session_name=False, printable=False):
-	client = Client(servers[index]["address"])
+def client_connection(index, server, security_policies_uri, auth=False, printable=False):
+	client = Client(server["address"])	
 	try:
-		best_endpoint = best_endpoint_selection(client, index, printable)
-		client = Client(best_endpoint.EndpointUrl)		
+		best_endpoint = best_endpoint_selection(client, server, security_policies_uri, printable)
+		
+		client = Client(best_endpoint.EndpointUrl)	
 		client.application_uri = "urn:freeopcua:client"
-
+		client.description = "OPCUA-Client-Kafka-Gateway"
+		
+		# Questo dovrebbe avvenire mediante codice
+		if auth: client = user_authentication(client)	
+		
 		policy = best_endpoint.SecurityPolicyUri.split('#')[1]		
 		if policy != "None": 
 			security_string = str(policy) + ',' + str(MessageSecurityMode(best_endpoint.SecurityMode).name) + ',client_certificate.pem' + ',client_key.pem' 
-			client.set_security_string(security_string)		
-
-		if auth: client = user_authentication(client)	
-		client.description = input("\nSession name: ") if session_name != False else "OPC UA - Kafka_Gateway_Client"
+			client.set_security_string(security_string)	
 		
-		client.connect()
-		clients_list[index] = client
-		working_servers[index] = servers[index]
+		client.connect()		
 		return client		
 	except:
 		return
 	#DOMANDA: Server Application Instance Certificate validation?	
 
-def servers_connection(servers):
-    # Servers connection
+def servers_connection(servers, security_policies_uri):
+	clients_list, working_servers = [None for s in servers],[None for s in servers]
 	for i,s in enumerate(servers):							
-		client = client_connection(i, printable=True) 
+		client = client_connection(i, s, security_policies_uri, auth=False, printable=True) 
 		if client == None: 
-			print(f"\nERROR: Server {s['address']} is not available!")
+			print(f"\nERROR: Cannot connect to server: {s['address']}!")
 		else:
+			clients_list[i] = client
+			working_servers[i] = s
 			print(f"\nConnected to Server {s['address']}!")
 	return clients_list, working_servers
 
 def read_nodes_from_json(servers):
     # Nodes setting function
 	nodes_to_handle = []
+	wrong_json = False
 	for s in servers:
 		nodes_to_read_s, nodes_to_monitor_s = [], []
 		for i,v in enumerate(s["Variables"]):
@@ -125,40 +126,52 @@ def read_nodes_from_json(servers):
 					raise KeyError
 			except KeyError:
 					print(f"\nError in server {s['address']} with the node {i+1}!")
+					wrong_json = True
 					# remove wrong nodes
 					s["Variables"].remove(v)
 								
 		nodes_to_handle.append(tuple([nodes_to_read_s, nodes_to_monitor_s]))
+	if wrong_json:
+		if(input("ERRORS in JSON configuration file.\nPress y to continue (ignoring wrong nodes), any other key to exit: ")!="y"):
+			sys.exit(0)
+		
 	return nodes_to_handle
 
-def signal_handler(sig, frame):
-	print("\nCLIENT STOPPED! (You pressed CTRL+C)")
-	for i, c in enumerate(clients_list):	
-		if c != None:
-			try:
-				if subs[i] != None: 
-					subs[i][0].delete()
-					print(f"\nSubscription {subs[i][0].subscription_id} deleted!")
-			except:
-				print(f"\nUnable to delete subscription {subs[i][0].subscription_id}!")		
-			try:
-				# "ns=0;i=2259" is ServerStatus node
-				c.get_node("ns=0;i=2259").get_data_value()	
-				c.disconnect()					
-				print(f"\nClient correctly disconnected from {servers[i]['address']}")
-			except:
-				print(f"\nServer {c.server_url.netloc} is probably down!")
-	os._exit(0) 
-	
+# removing invalid (not in address space) nodes from running servers 
+def removing_invalid_nodes(clients_list, working_servers, nodes_to_handle):
+	# recall this function should permit to read (Polling service) or monitor (Monitored Items service) previously missing nodes in servers address allowing to add them subsequently (anyway they have to appear in json file).
+	for i, s in enumerate(working_servers):
+		if s != None:
+			# monitored items nodes 
+			for n in nodes_to_handle[i][1]:
+				try:
+					clients_list[i].get_node(n["node"]).get_data_value()	
+				except:
+					print(f"\n\nWARNING: Node {n['node']} of server {s['address']} for Monitored Item service removed because it's not in address space.\n\n")
+					nodes_to_handle[i][1].remove(n)
+			
+			# polling nodes 
+			for n in nodes_to_handle[i][0]:
+				try:
+					clients_list[i].get_node(n).get_data_value()	
+				except:
+					print(f"\n\nWARNING: Node {n['node']} of server {s['address']} for Polling service removed because it's not in address space.\n\n")
+					nodes_to_handle[i][0].remove(n)
+	return nodes_to_handle					
+
 class SubHandler(object):
+	def __init__(self, clients_list, queues):
+		self.clients_list = clients_list
+		self.queues = queues
+		
 	def datachange_notification(self, node, val, data):
 		try:
-			i = [c.uaclient if c is not None else None for c in clients_list].index(node.server)
+			i = [c.uaclient if c is not None else None for c in self.clients_list].index(node.server)
 			notification = {"node":node, "value":data.monitored_item.Value.Value, "status":data.monitored_item.Value.StatusCode, "sourceTimestamp":data.monitored_item.Value.SourceTimestamp, "serverTimestamp":data.monitored_item.Value.ServerTimestamp}
-			queues[i].put(notification)
+			self.queues[i].put(notification)
 		except ValueError:
-			pass		
-
+			pass						
+			
 def _create_monitored_items(sub, dirty_nodes, attr, queuesize=0):
 	mirs = []
 	for dirty_node in dirty_nodes:
@@ -181,29 +194,9 @@ def _create_monitored_items(sub, dirty_nodes, attr, queuesize=0):
 	mids = sub.create_monitored_items(mirs)
 	return mids
 
-# removing invalid (not in address space) nodes from running servers 
-def removing_invalid_nodes(working_servers):
-	# following line should permit to read (Polling service) or monitor (Monitored Items service) previously missing nodes in servers address allowing to add them subsequently (anyway they have to appear in json file).
-	nodes_to_handle = read_nodes_from_json(servers)
-	for i, s in enumerate(working_servers):
-		if s != None:
-			# monitored items nodes 
-			for n in nodes_to_handle[i][1]:
-				try:
-					clients_list[i].get_node(n["node"]).get_data_value()	
-				except:
-					print(f"\n\nWARNING: Node {n['node']} of server {s['address']} for Monitored Item service removed because it's not in address space.\n\n")
-					nodes_to_handle[i][1].remove(n)
-			
-			# polling nodes 
-			for n in nodes_to_handle[i][0]:
-				try:
-					clients_list[i].get_node(n).get_data_value()	
-				except:
-					print(f"\n\nWARNING: Node {n['node']} of server {s['address']} for Polling service removed because it's not in address space.\n\n")
-					nodes_to_handle[i][0].remove(n)
 					
-def sub_and_monitored_items_creation(subs, queues):
+def sub_and_monitored_items_creation(clients_list, working_servers, nodes_to_handle, subs, queues, publishingInterval):	
+	handler = SubHandler(clients_list, queues)		
 	# we are assuming a subscription per client			
 	for i, s in enumerate(working_servers):
 		if s != None and subs[i]==None and len(nodes_to_handle[i][1]) > 0:
@@ -211,9 +204,10 @@ def sub_and_monitored_items_creation(subs, queues):
 			sub = clients_list[i].create_subscription(publishingInterval, handler)
 			handle = _create_monitored_items(sub, nodes_to_handle[i][1], ua.AttributeIds.Value)	
 			subs[i] = tuple((sub, handle))
-			queues[i] = queue.Queue()		
+			queues[i] = queue.Queue()			
+	return subs, queues
 	
-def check_servers_status(servers):
+def check_servers_status(servers, clients_list, working_servers, nodes_to_handle, subs, queues, security_policies_uri, publishingInterval):
 	for i, s in enumerate(servers):
 		try:
 			# "ns=0;i=2259" is ServerStatus node
@@ -227,20 +221,21 @@ def check_servers_status(servers):
 			# check if previously down servers are now up
 			if working_servers[i] == None:
 				# best_endpoint_selection and client_connection  
-				client = client_connection(i, printable=False)
-				if client != None: print(f"\nCONNECTED to Server {s['address']}!")				
+				client = client_connection(i, s, security_policies_uri, auth=False, printable=False)
+				if client != None: 
+					clients_list[i] = client
+					working_servers[i] = s
+					print(f"\nCONNECTED to Server {s['address']}!")				
 				# sub and monitored_items creation 
-				sub_and_monitored_items_creation(subs, queues)				
-				
+				subs, queues = sub_and_monitored_items_creation(clients_list, working_servers, nodes_to_handle, subs, queues, publishingInterval)
+					
 	# this way we remove wrong nodes from NEW servers and it should remove a node if exists no more (has been deleted).
-	removing_invalid_nodes(working_servers)
+	nodes_to_handle = removing_invalid_nodes(clients_list, working_servers, nodes_to_handle)
+	return clients_list, working_servers, nodes_to_handle, subs, queues
 	
 
-def polling_and_monitoring_service (working_servers, nodes_to_handle, clients_list, queues):
+def polling_and_monitoring_service (servers, clients_list, working_servers, nodes_to_handle, queues):
 	# READ SERVICE e MONITORED ITEMS notifications function
-	global counter
-	counter+=1
-	print(f"\n{'*'*15} ITERATION n. {counter} {'*'*15}")
 	for i, s in enumerate(working_servers): 	
 		print(f"\n\n{'-'*10} SERVER: {servers[i]['address']} {'-'*10}")
 		if s != None:		
@@ -257,7 +252,7 @@ def polling_and_monitoring_service (working_servers, nodes_to_handle, clients_li
 						ServerTimestamp: {data_v.ServerTimestamp}
 						"""))
 					except:
-						# credo che si possa verificare solo se succede qualcosa al server o viene rimosso il nodo
+						# this could be true only if something happens to server or a node has been removed.
 						print(f"\nDetected a problem with node {n} from this server.")							
 			else:
 				print("NO nodes to read for this server!")
@@ -289,6 +284,7 @@ def polling_and_monitoring_service (working_servers, nodes_to_handle, clients_li
 				working_servers.append(s)"""
 		else:
 			print("Server is DOWN!")
+	return queues
 
 # This function is a patching of _call_publish_callback function from UaClient module to handle unexpected server closing.
 def our_call_publish_callback(self, future):
@@ -324,53 +320,8 @@ def our_call_publish_callback(self, future):
 	try:
 		callback(response.Parameters)
 	except Exception:  # we call client code, catch everything!
-		self.logger.exception("Exception while calling user callback: %s")
+		self.logger.exception("Exception while calling user callback: %s")	
 
-			
-
-if __name__ == "__main__":	
-	ua_client.UaClient._call_publish_callback = our_call_publish_callback	
-	logging.getLogger('opcua').setLevel(logging.CRITICAL)
-	
-	sys.path.insert(0, "..")
-	signal.signal(signal.SIGINT, signal_handler)
-
-	with open("config.json") as f:
-		data = json.load(f)
-
-	servers = data["Servers"]
-	kafka_addr = data["KafkaServer"]
-	pollingRate = data["PollingRate"]
-	publishingInterval = data["PublishingInterval"]
-
-	# DA TOGLIERE: con la security policy None e Anonymous funziona, con il resto altri server oltre al Sample NO (forse per via del certificato?)!
-	security_policies_uri = ['http://opcfoundation.org/UA/SecurityPolicy#None']
-	#security_policies_uri=['http://opcfoundation.org/UA/SecurityPolicy#None', 'http://opcfoundation.org/UA/SecurityPolicy#Basic128Rsa15', 'http://opcfoundation.org/UA/SecurityPolicy#Basic256', 'http://opcfoundation.org/UA/SecurityPolicy#Basic256Sha256']
-
-	#Server connection
-	clients_list, working_servers = [None for s in servers],[None for s in servers]
-	servers_connection(servers)
-
-	print("\n"*3 , f"{'-'*60}")
-	
-	#Variables Management
-	nodes_to_handle = read_nodes_from_json(servers)
-
-	# MONITORED ITEM function
-	handler = SubHandler()				#una per ogni monitoredItem?
-	
-	removing_invalid_nodes(working_servers)
-	
-	subs = [None for i in clients_list]
-	queues = [None for i in clients_list]
-	
-	sub_and_monitored_items_creation(subs, queues)
-			
-	time.sleep(0.1)
-
-	counter = 0 
-	while(True):
-		#Check on new working servers and servers that are working no more.
-		check_servers_status(servers)
-		polling_and_monitoring_service(working_servers, nodes_to_handle, clients_list, queues)
-		time.sleep(pollingRate)
+ua_client.UaClient._call_publish_callback = our_call_publish_callback
+logging.getLogger('opcua').setLevel(logging.CRITICAL)	
+sys.path.insert(0, "..")
